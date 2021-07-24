1045895d5a95e9cd53355a9d674bd7c6e9a2d97d
mingjiedege <87758048+mingjiedege@users.noreply.github.com>
2021-07-24 19:38:05 +0800

    Add files via upload
    字符从两边到中间

--- test.c
+++ test.c
@@ -1,70 +1,25 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
-//猜数字游戏
-//1。电脑生成一个随机数
-//2.猜数字
 #include <stdio.h>
 #include <string.h>
+#include <Windows.h>
 #include <stdlib.h>
-#include <time.h>
-
-void menu()
-{
-	printf("*********************************\n");
-	printf("******    1. play  0.exit  ******\n");
-	printf("*********************************\n");
-}
-void game()
-{
-	int ret = 0;
-	int guess = 0;
-//时间戳：当前计算机的时间减去计算机的起始时间（1970.1.1.0:0:0）=（xxxx秒）
-	//拿时间戳来设置随机数的生成起点
-	//time函数  返回值是time_t
-    ret=rand()%100+1;//生成1-100之间随机数
-	//printf("%d\n", ret);
-	while (1)
-	{
-		printf("请猜数字:>");
-		scanf("%d", &guess);
-		if (guess > ret)
-		{
-			printf("猜大了\n");
-		}
-		else if (guess < ret)
-		{
-			printf("猜小了\n");
-
-		}
-		else
-		{
-			printf("恭喜你，猜对了！！！\n");
-			break;
-		}
-	}
-}
+//编写代码，演示多个字符从两端移动，向中间汇聚
 int main()
 {
-	srand((unsigned int)time(NULL));//放在主函数设置一次就行了
-	int input = 0;
-
-	do
+	char arr1[] = "welcome to bit!!!!!";
+	char arr2[] = "###################";
+	int left = 0;
+	int right = strlen(arr1) - 1;
+	while (left <= right)
 	{
-		menu();
-		printf("请选择:>");
-		scanf("%d", &input);
-		switch (input)
-		{
-		case 1:
-			game();
-			break;
-		case 0:
-			printf("退出游戏\n");
-			break;
-		default:
-			printf("选择错误\n");
-			break;
-
-		}
-	} while (input);
+		arr2[left] = arr1[left];
+		arr2[right] = arr1[right];
+		printf("%s\n", arr2);
+		Sleep(1000);
+		system("cls");
+		left++;
+		right--;
+	}
+	printf("%s\n", arr2);
 	return 0;
 }
