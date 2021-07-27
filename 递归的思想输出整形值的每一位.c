5ba30e591ba3e2d75b208b2ad7610b3c7822b216
mingjiedege <87758048+mingjiedege@users.noreply.github.com>
2021-07-27 20:11:17 +0800

    Add files via upload

---
+++ 递归的思想输出整形值的每一位.c
@@ -0,0 +1,18 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+void print(int n)
+{
+	if (n > 9)
+	{
+		print(n / 10);
+	}
+	printf("%d ", n % 10);
+}
+int main()
+{
+	unsigned int num = 0;
+	scanf("%d", &num);
+	print(num);
+	return 0;
+}
