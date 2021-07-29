b5cbb4402469688d1f9505c3053db40f7c383f0b
mingjiedege <87758048+mingjiedege@users.noreply.github.com>
2021-07-29 19:56:36 +0800

    Add files via upload

---
+++ switchcase语句.c
@@ -0,0 +1,22 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int day = 0;
+	scanf("%d", &day);
+	switch (day)
+	{
+	case 1:
+	case 2:
+	case 3:
+	case 4:
+	case 5:
+		printf("工作日\n");
+	case 6:
+	case 7:
+		printf("休息日\n");
+		break;
+	default:printf("输入错误\n");
+	}
+	return 0;
+}
