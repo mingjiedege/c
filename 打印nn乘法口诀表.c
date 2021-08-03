cc369cc1347d67447fe2f0cabbfb5b8fa91bda08
mingjiedege <87758048+mingjiedege@users.noreply.github.com>
2021-08-03 21:31:14 +0800

    Add files via upload

---
+++ 打印nn乘法口诀表.c
@@ -0,0 +1,22 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+void print_table(int n)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 1; i <= n; i++)
+	{
+		for (j = 1; j <=i; j++)
+		{
+			printf("%d*%d=%-3d ", i, j, i* j);
+		}
+		printf("\n");
+	}
+}
+int main()
+{
+	int n = 0;
+	scanf("%d", &n);
+	print_table(n);
+	return 0;
+}
