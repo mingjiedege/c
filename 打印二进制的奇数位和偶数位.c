a0d6ccc795bb0c4dbf5abe88ae69fbeb2d394637
mingjiedege <87758048+mingjiedege@users.noreply.github.com>
2021-08-03 21:30:01 +0800

    Add files via upload

---
+++ 打印二进制的奇数位和偶数位.c
@@ -0,0 +1,26 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+void print(int m)
+{
+	int i = 0;
+	printf("奇数位:\n");
+	for (i = 30; i >= 0; i -= 2)
+	{
+		printf("%d ", (m >> i) & 1);//奇数位
+	}
+	printf("\n");
+	printf("偶数位:\n");
+	for (i = 31; i >= 1; i -= 2)
+	{
+		printf("%d ", (m >> i) & 1);
+	}
+	printf("\n");
+}
+int main()
+{
+	int m = 0;
+	scanf("%d", &m);
+	print(m);
+	return 0;
+
+}
