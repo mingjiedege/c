e5b91607cb9b0f132bd1d26d8a97d9e577dd9ca0
mingjiedege <87758048+mingjiedege@users.noreply.github.com>
2021-09-05 22:26:51 +0800

    Add files via upload

---
+++ 猜凶手.c
@@ -0,0 +1,31 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+////日本某地发生了一件谋杀案，警察通过排查确定了杀人凶手必为4个嫌疑犯的一个
+////以下为4个嫌疑犯的供词：
+//	//A说：不是我(1/0)
+//	//B说：是C
+//	//C说：是D
+//	//D说：C在胡说
+//	//已知3个人说了真话，1个人说的是假话
+//	//现在根据这些信息，编写一个程序来确定谁是凶手
+//	int killer = 0;
+//	for (killer = 'a'; killer <= 'd'; killer++)
+//	{
+//		if ((killer != 'a') + (killer == 'c') + (killer == 'd') + (killer != 'd') == 3)
+//		{
+//			printf("killer=%c\n", killer);
+//		}
+//	}
+//	return 0;
+//
+//}
+//赛马问题
+//36匹马，6个跑道，请赛马确定，36匹马中的前3名
+//请问最少比赛几次
+int main()
+{
+	6+3+1+1
+	return 0;
+}
