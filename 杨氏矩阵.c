3e6032a5829688d75f9a8dd17029bbcef059b193
mingjiedege <87758048+mingjiedege@users.noreply.github.com>
2021-10-03 00:13:26 +0800

    Add files via upload

---
+++ 杨氏矩阵.c
@@ -0,0 +1,81 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+//有一个数字矩阵，矩阵的每行从左到右是递增的，请编写程序在这样的矩阵中查找某个数字是否存在
+//要求时间复杂度小于O（N）
+#include<stdio.h>
+//int FindNum(int arr[3][3], int k, int row, int col)
+//{
+//	int x = 0;
+//	int y = col - 1;
+//	while (y>=0&&x<=row-1)
+//	{
+//		if (arr[x][y] > k)
+//		{
+//			y--;
+//		}
+//		else if (arr[x][y] < k)
+//		{
+//			x++;
+//		}
+//		else
+//		{
+//			return 1;
+//		}
+//	}
+//}
+//int main()
+//{
+//	int arr[3][3] = { {1,2,3},{4,5,6},{7,8,9} };
+//	int k = 7;
+//	int ret = FindNum(arr, k,3,3);
+//	if (ret == 1)
+//	{
+//		printf("找到了\n");
+//	}
+//	else
+//	{
+//		printf("找不到\n");
+//	}
+//	return 0;
+//}
+//要返回k所在位置
+int FindNum(int arr[3][3], int k, int* px, int* py)
+{
+	int x = 0;
+	int y = *py - 1;
+	while (x <= *px - 1 && y >= 0)
+	{
+		if (arr[x][y] > k)
+		{
+			y--;
+		}
+		else if (arr[x][y] < k)
+		{
+			x++;
+		}
+		else
+		{
+			*px = x;
+			*py = y;
+			return 1;
+		}
+	}
+	return 0;
+}
+int main()
+{
+	int arr[3][3] = { {1,2,3},{4,5,6},{7,8,9} };
+	int k = 7;
+	int x = 3;
+	int y = 3;
+	int ret = FindNum(arr, k, &x, &y);
+    if (ret == 1)
+	{
+		printf("找到了\n");
+		printf("下标是：%d %d\n", x, y);
+	}
+	else
+	{
+		printf("找不到\n");
+	}
+	return 0;
+}
